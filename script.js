123db4885328dbe98dfb9dd0e8fab2719289f369
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-10-27 12:51:10 +0700

    Добавлен вывод БИО и стили

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let body = document.body;
 
-fetch('https://api.github.com/users/DmBorozdin')
+fetch('https://api.github.com/users/DMBorozdin')
   .then(info => info.json())
   .then(json => {
     console.log(json.avatar_url);
@@ -13,13 +13,19 @@ fetch('https://api.github.com/users/DmBorozdin')
     userName.href = json.html_url;
     userName.innerHTML = json.name;
     userName.classList.add('link');
-    console.log(userName);
     console.log(json.name);
     body.appendChild(userName);
 
-    let userBio = json.bio;
-    console.log(userBio);
+    let userBio = document.createElement('p');
+    if (json.bio != null) {
+      userBio.innerHTML = json.bio;
+    } else {
+      userBio.innerHTML = 'Информация о пользователе не доступна';
+    }
+    userBio.classList.add('bio');
+    console.log(json.bio);
+    body.appendChild(userBio);
 
     console.log(json.html_url);
   })
-  .catch(err => console.log(err));
+  .catch(err =>  alert('Информация о пользователе не доступна'));
